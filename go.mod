fcec88b95d6691846dc81a005842d8c3677266e1
E. Krebs <ekrebs@outlook.com>
2021-05-17 12:37:28 -0500

    use more friendly dialogs

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/something-something-something/go-ffmpeg-ui
 
 go 1.16
 
-require fyne.io/fyne/v2 v2.0.3
+require (
+	fyne.io/fyne/v2 v2.0.3
+	github.com/ncruces/zenity v0.7.3
+)
--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	"strings"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
-	"fyne.io/fyne/v2/dialog"
+	//"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-
+	"github.com/ncruces/zenity"
+	"os/exec"
 )
 
 func main() {
-	ffmpegLocation:=binding.NewString()
+	ffmpegLocation := binding.NewString()
 	ffmpegLocation.Set("")
 
 	importFileName := binding.NewString()
@@ -29,98 +30,87 @@ func main() {
 	w := a.NewWindow("ffmpeg")
 	c := container.New(layout.NewGridLayout(2))
 
-	c.Add(	widget.NewButton("Click to select ffmpeg Executable", func(){
-		d:=dialog.NewFileOpen(func(u fyne.URIReadCloser,err error){
-			if err == nil && u != nil {
-				ffmpegLocation.Set(u.URI().Path())	
-			}
-		},w)
-		setDialogSizeAndShow(d)
+	c.Add(widget.NewButton("Click to select ffmpeg Executable", func() {
+		loc, err := zenity.SelectFile()
+		if err == nil {
+			ffmpegLocation.Set(loc)
+		}
 	}))
 	c.Add(widget.NewLabelWithData(ffmpegLocation))
 	c.Add(
 		widget.NewButton("import", func() {
-		
-			importFile(func(u fyne.URIReadCloser, err error) {
-
-				if err == nil && u != nil {
-					importFileName.Set(u.URI().Path())
-				}
-
-			}, w)
-
-		}))
-		c.Add(widget.NewLabelWithData(importFileName))
-		
-		c.Add(widget.NewButton("export", func() {
-			exportFile(func(u fyne.URIWriteCloser, err error) {
-
-				if err == nil && u != nil {
-					exportFileName.Set(u.URI().Path())
-				}
-
-			}, w)
-
-		}))
-		c.Add(widget.NewLabelWithData(exportFileName))
-		
-		c.Add(widget.NewButton("Run conversion", func() {
-			fmt.Println("A")
-			impath,imerr:=importFileName.Get()
-			outpath,outerr:=exportFileName.Get()
-			ffmpegLoc,ffmpegLocErr:=ffmpegLocation.Get()
-			
-			if imerr==nil && outerr==nil && ffmpegLocErr==nil && ffmpegLoc!="" {
-				fmt.Println("B")
-				cmd:=exec.Command(ffmpegLoc,"-y","-i",impath,outpath)
-				ffmpegStatus.Set("Running")
-				fmt.Println("C")
-				ffmpegText,err:=cmd.CombinedOutput();
-				fmt.Println("D")
-				fmt.Printf("%s\n", ffmpegText)
-				//cmd.Run()
-				if err!=nil{
-					fmt.Println("ERROR")
-					fmt.Println(err)
-				}
-				ffmpegStatus.Set("Finnished")
-				a.SendNotification(fyne.NewNotification("ffmpeg ui wraper","Finished converting"))
-			} else {
-				fmt.Println("X")
+			imFile, err := zenity.SelectFile()
+			if err == nil {
+				importFileName.Set(imFile)
 			}
-			fmt.Println("Z")
 		}))
-		c.Add(widget.NewLabelWithData(ffmpegStatus))
+	c.Add(widget.NewLabelWithData(importFileName))
+
+	c.Add(widget.NewButton("export", func() {
+
+		outFile, err := zenity.SelectFileSave(zenity.Filename("ffmpeg.mp4"))
+		if err == nil {
+			exportFileName.Set(outFile)
+		}
+
+	}))
+	c.Add(widget.NewLabelWithData(exportFileName))
+
+	c.Add(widget.NewButton("Run conversion", func() {
+		fmt.Println("A")
+		impath, imerr := importFileName.Get()
+		outpath, outerr := exportFileName.Get()
+		ffmpegLoc, ffmpegLocErr := ffmpegLocation.Get()
+		
+		if imerr == nil && outerr == nil && ffmpegLocErr == nil && ffmpegLoc != "" {
+			fmt.Println("B")
+			cmd := exec.Command(strings.TrimSpace(ffmpegLoc), "-y", "-i", strings.TrimSpace(impath), strings.TrimSpace(outpath))
+			ffmpegStatus.Set("Running")
+			fmt.Println("C")
+			ffmpegText, err := cmd.CombinedOutput()
+			fmt.Println("D")
+			fmt.Printf("%s\n", ffmpegText)
+			//cmd.Run()
+			if err != nil {
+				fmt.Println("ERROR")
+				fmt.Println(err)
+			}
+			ffmpegStatus.Set("Finnished")
+			a.SendNotification(fyne.NewNotification("ffmpeg ui wraper", "Finished converting"))
+		} else {
+			fmt.Println("X")
+		}
+		fmt.Println("Z")
+	}))
+	c.Add(widget.NewLabelWithData(ffmpegStatus))
 	w.SetContent(c)
 
-		
 	w.Resize(fyne.Size{
-		Width: 800,
+		Width:  800,
 		Height: 800,
 	})
 	//w.SetFixedSize(true)
 	//w.CenterOnScreen()
 	w.ShowAndRun()
-	
-	
-}
-func setDialogSizeAndShow(d dialog.Dialog){
-	d.Resize(fyne.Size{
-		Width: 800,
-		Height: 800,
-	})
-	d.Show()
-}
-
-func importFile(f func(fyne.URIReadCloser, error), win fyne.Window) {
-	d:=dialog.NewFileOpen(f,win)
-	setDialogSizeAndShow(d)
-
 
 }
-func exportFile(f func(fyne.URIWriteCloser, error), win fyne.Window) {
-	d:=dialog.NewFileSave(f,win)
-	d.SetFileName("ffmpeg.mp4")
-	setDialogSizeAndShow(d)
 
-}
+// func setDialogSizeAndShow(d dialog.Dialog){
+// 	d.Resize(fyne.Size{
+// 		Width: 800,
+// 		Height: 800,
+// 	})
+// 	d.Show()
+// }
+
+// func importFile(f func(fyne.URIReadCloser, error), win fyne.Window) {
+// 	d:=dialog.NewFileOpen(f,win)
+// 	setDialogSizeAndShow(d)
+
+// }
+// func exportFile(f func(fyne.URIWriteCloser, error), win fyne.Window) {
+// 	d:=dialog.NewFileSave(f,win)
+// 	d.SetFileName("ffmpeg.mp4")
+// 	setDialogSizeAndShow(d)
+
+// }
